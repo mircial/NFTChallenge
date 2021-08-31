7e4d3e7974284b15ecfd204fef3350003908aa81
leo <leo@github.com>
2021-08-31 23:17:05 +0800

    fix parse issue

--- contracts/NFTChallengeCore.sol
+++ contracts/NFTChallengeCore.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
- 
 pragma solidity ^0.8.0;
 
 import "../interfaces/INFTChallengeCore.sol";
 import "../interfaces/IERC721.sol";
 import "../libraries/Address.sol";
-import "./NFTCollectorERC721.sol";
  
-contract NFTChallengeCore is INFTChallengeCore{​​​​
+contract NFTChallengeCore is INFTChallengeCore {
     using Address for address;  
     mapping(address => mapping(uint256 => address)) private _StoreTokenId;
     
@@ -15,12 +13,12 @@ contract NFTChallengeCore is INFTChallengeCore{​​​​
         address contract_address, 
         address user, 
         uint256 tokenId
-    ) external override returns(bool){​​​​
+    ) external override returns(bool){
 
         require(contract_address.isContract() == true, 'contract address does not exist!');
         require(IERC721(contract_address).ownerOf(tokenId) == user, 'Invalid user');
         require(_StoreTokenId[contract_address][tokenId] == address(0),' tokenId used!');
         _StoreTokenId[contract_address][tokenId] = user;
         return true;
-    }​​​​
+    }
 }
