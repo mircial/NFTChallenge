4c6e776bbda9aa192c8e887d1fa330f8f294187c
leo <leo@github.com>
2021-08-31 22:56:57 +0800

    add the core contract

---
+++ interfaces/INFTChallengeCore.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ *  NFTChallenge core code interface
+ */
+interface INFTChallengeCore {
+    
+    function NFTExist(
+        address contract_address, 
+        address user,
+        uint256 tokenId
+        ) external returns(bool);
+
+}
---
+++ test/ERC721.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "../interfaces/IERC721.sol";
+
+/**
+ * refer to OpenZeppelin/openzeppelin-contracts
+ */
+contract ERC721 is INFTChallengeERC721 {
+    
+}
